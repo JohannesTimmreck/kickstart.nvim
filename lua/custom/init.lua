397dfd06a90e3f778a5b5861bcb7506c04bb9f25
Johannes Timmreck <johannes.timmreck@mhp.com>
2025-07-04 09:22:44 +0200

    feat: small update

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+require 'custom.copilot'
+
+return {
+  'natecraddock/workspaces.nvim',
+  'ThePrimeagen/vim-be-good',
+  -- Optional: Add lazy-loading if desired
+  cmd = 'VimBeGood',
+}
